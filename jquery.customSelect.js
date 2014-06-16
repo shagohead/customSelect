396735b56ffb8cef1fd7ab2427f9ecb1fbb201a8
LastDanmer <html.ru@gmail.com>
2014-06-17 03:34:18 +0600

    refactor

--- jquery.customSelect.js
+++ jquery.customSelect.js
@@ -1,269 +1,172 @@
-/**
+/*!
  * Plugin for customize default selects
  * @author Anton Vahmin (html.ru@gmail.com)
- * @copyright Clever Site Studio (http://clever-site.ru)
- * @version 2.5.1
+ * @version 3.0.0
  */
 
-function clone(obj) {
-    var target = {};
-    for (var i in obj) {
-        if (obj.hasOwnProperty(i)) {
-            target[i] = obj[i];
+$.widget('custom.customSelect', {
+    options: {
+        tagName: 'div',
+        closeOthers: true,
+        copyClasses: true,
+        copyData: true,
+        wrapIn: [],
+        optionTemplate: null,
+
+        select: null,
+        click: null,
+        open: null,
+        close: null
+    },
+
+    _create: function() {
+        this.isDisabled = false;
+        var containerClass = 'customSelectContainer';
+        if (this.element.attr('disabled')) {
+            containerClass += ' customSelectContainer_state_disabled';
+            this.isDisabled = true;
+        }
+
+        if (this.element.attr('class')) {
+            containerClass += ' ' + this.element.attr('class');
+        }
+
+        var itemsList = [];
+        var selectedIndex = 0;
+        this.element.find('option').each(function(index) {
+            var item = {
+                text: $(this).text(),
+                value: $(this).attr('value')
+            };
+            var data = $(this).data();
+            for (var i in data) {
+                item[i] = data[i];
+            }
+            itemsList.push(item);
+            if ($(this).is(':selected')) {
+                selectedIndex = index;
+            }
+        });
+        this.itemsList = itemsList;
+        this.selectedIndex = selectedIndex;
+
+        this.domContainer = $('<' + this.options.tagName + ' class="' + containerClass + '" />');
+        this.element.hide();
+        this.element.after(this.domContainer);
+
+        this.domText = $('<' + this.options.tagName + ' class="customSelectText" />');
+        this.domContainer.append(this.domText);
+        for (i in this.options.wrapIn) {
+            var wrapper = $('<' + this.options.tagName + ' class="customSelect' +
+                this.options.wrapIn[i] + '" />');
+            this.domText.append(wrapper);
+            this.domText = wrapper;
+        }
+
+        this.domOptions = $('<' + this.options.tagName + ' class="customSelectOptions" />');
+        this.domScroll = $('<' + this.options.tagName + ' class="customSelectScroll" />');
+        this.domOptions.append(this.domScroll);
+        this.domContainer.append(this.domOptions);
+
+
+        this._appendItems();
+        this.domOptions.css('width', this.domContainer.width());
+        this.domOptions.hide();
+
+        var _this = this;
+        this.domText.bind('click', function() {
+            if (_this.isDisabled === false) {
+                _this._trigger('click', this);
+                _this.toggle();
+            }
+        });
+        this.domScroll.on('mousewheel', function() {
+            this.scrollApi.scrollByY(event.deltaY);
+        });
+    },
+
+    _appendItems: function() {
+        var _this = this;
+        this.domItems = [];
+        this.domText.unbind('click');
+//        $(document).off(this.domItems);
+        for (var i = 0; i < this.itemsList.length; i++) {
+            var selected = '';
+            if (i == this.selectedIndex) {
+                selected = ' selected';
+                $(this).val(this.itemsList[i].value);
+                this.domText.html(this.itemsList[i].text);
+            }
+            var item = $('<' + this.options.tagName + ' class="customSelectOption' + selected + '" data-value="' +
+                this.itemsList[i].value + '">' + this.itemsList[i].text + '</' + this.options.tagName + '>');
+            if (this.options.optionTemplate) {
+                item = this.options.optionTemplate.call(this, item, this.itemsList[i]);
+            }
+
+            item.click(function () {
+                if (_this.isDisabled === false) {
+                    _this._trigger('select', this);
+                    $(_this).val($(this).data('value'));
+                    var text = ($(this).data('text') != '') ? $(this).data('text') : $(this).text();
+                    _this.domText.html(text);
+                    _this.close();
+                }
+            });
+            this.domScroll.append(item);
+            this.domItems.push(item);
+        }
+        this.domScroll.jScrollPane();
+        this.scrollApi = this.domScroll.data('jsp');
+
+        $(document).click(function() {
+            _this.close();
+        });
+        this.domContainer.click(function(event) {
+            event.stopPropagation();
+        });
+        $(document).keyup(function(event) {
+            if (event.keyCode == 27) {
+                _this.close();
+            }
+        });
+    },
+
+    setItems: function(items) {
+        var data_items = [];
+        for (i in items) {
+            var topush = (typeof items[i] == 'object') ? {
+                text: items[i][1],
+                value: items[i][0]
+            } : {
+                text: items[i],
+                value: items[i]
+            };
+            data_items.push(topush);
+        }
+        this.itemsList = data_items;
+        this._appendItems();
+    },
+
+    open: function() {
+        this.domContainer.addClass('customSelectOpen');
+        this._trigger('open');
+        if (this.options.closeOthers) {
+            $('.customSelectOptions').hide();
+        }
+        this.domOptions.show();
+    },
+
+    close: function() {
+        this.domContainer.removeClass('customSelectOpen');
+        this._trigger('close', this);
+        this.domOptions.hide();
+    },
+
+    toggle: function() {
+        if (this.domOptions.is(':visible')) {
+            this.close();
+        } else {
+            this.open();
         }
     }
-    return target;
-}
-
-(function($) {
-
-    var methods = {
-        init: function(settings) {
-            settings = $.extend({
-                element: 'div',
-                maxRows: false,
-                template: false,
-                closeOthers: true,
-                copyClasses: true,
-                copyData: true,
-                // events callbacks:
-                onSelect: false,
-                onClick: false,
-                onOpen: false,
-                onClose: false
-            }, settings);
-
-            return this.each(function(index, element) {
-                var object = this;
-                var data = $(object).data();
-                data.settings = clone(settings);
-                data.settings.callbacks = clone(settings.callbacks);
-                data.dom = {};
-
-                var currentSelect = $(this);
-                var container_class = 'customSelectContainer';
-                data.disabled = false;
-                data.name = currentSelect.attr('name');
-
-                if (currentSelect.attr('disabled')) {
-                    container_class += ' customSelectContainer_state_disabled';
-                    data.disabled = true;
-                }
-
-                if (currentSelect.attr('class')) {
-                    container_class += ' ' + currentSelect.attr('class');
-                }
-
-                data.items = [];
-                data.dom.items = [];
-                data.selected = {};
-
-                currentSelect.find('option').each(function() {
-                    if ($(this).is(':selected')) {
-                        data.selected = {
-                            text: $(this).text(),
-                            value: $(this).attr('value')
-                        };
-                    }
-                    data.items.push({
-                        text: $(this).text(),
-                        value: $(this).attr('value')
-                    });
-                });
-
-                data.dom.text = $('<' + data.settings.element + ' class="customSelectText" />');
-                data.dom.left = $('<' + data.settings.element + ' class="customSelectLeft" />');
-                data.dom.right = $('<' + data.settings.element + ' class="customSelectRight" />');
-                data.dom.back = $('<' + data.settings.element + ' class="customSelectBack" />');
-                data.dom.arrow = $('<' + data.settings.element + ' class="customSelectArrow" />');
-                data.dom.scroll = $('<' + data.settings.element + ' class="customSelectScroll" />');
-                data.dom.options = $('<' + data.settings.element + ' class="customSelectOptions" />');
-                data.dom.container = $('<' + data.settings.element + ' class="' + container_class + '" />');
-                data.dom.input = $('<input type="hidden" name="' + data.name +
-                    '" value="" class="customSelectValue" />');
-
-                currentSelect.hide();
-                currentSelect.removeAttr('name');
-                currentSelect.after(data.dom.container);
-                data.dom.container.append(data.dom.input);
-                data.dom.container.append(data.dom.text);
-                data.dom.text.append(data.dom.left);
-                data.dom.left.append(data.dom.right);
-                data.dom.right.append(data.dom.back);
-                data.dom.back.append(data.dom.arrow);
-
-                if (data.settings.template) {
-                    data.dom.textElement = $(data.settings.template);
-                    data.dom.arrow.append(data.dom.textElement);
-                } else {
-                    data.dom.textElement = data.dom.arrow;
-                }
-
-                data.dom.container.append(data.dom.options);
-                data.dom.options.append(data.dom.scroll);
-
-                $(object).data(data);
-                methods.setItems.call(object);
-                methods.applyEvents.call(object);
-                data.dom.options.css('width', data.dom.container.width());
-                data.dom.options.hide();
-            });
-        },
-
-        data: function() {
-            return $(this).data();
-        },
-
-        setOption: function() {
-            var data = $(this).data();
-            return data.settings[arguments[0]] = arguments[1];
-        },
-
-        setItems: function() {
-            var data = $(this).data();
-
-            data.index = 0;
-            data.selected = (data.selected) ? data.selected : data.items[0];
-            data.dom.input.val(data.selected.value);
-            data.dom.textElement.text(data.selected.text);
-            data.dom.items = [];
-
-            for (var i = 0; i < data.items.length; i++) {
-                var option = $('<' + data.settings.element + ' class="customSelectOption" rel="' +
-                    data.items[i].value + '">' + data.items[i].text + '</' + data.settings.element + '>');
-                data.dom.items.push(option[0]);
-            }
-
-            data.dom.scroll.html('');
-            for (var i = 0; i < data.dom.items.length; i++) {
-                data.dom.scroll.append(data.dom.items[i]);
-            }
-
-            if (data.settings.maxRows !== false && data.items.length > data.settings.maxRows) {
-                data.dom.scroll.css(
-                    'height',
-                    (data.dom.scroll.children(':first-child').height() * data.settings.maxRows) + 'px');
-                methods.applyScroll(data.dom.scroll);
-            }
-            $(this).data(data);
-        },
-
-        setItemsJSON: function(items) {
-            var data_items = [];
-            for (i in items) {
-                var topush = (typeof items[i] == 'object') ? {
-                    text: items[i][1],
-                    value: items[i][0]
-                } : {
-                    text: items[i],
-                    value: items[i]
-                };
-                data_items.push(topush);
-            }
-            $(this).data('items', data_items);
-            methods.setItems.call(this);
-            methods.applyEvents.call(this);
-        },
-
-        applyScroll: function(scroll) {
-            if (!jQuery().jScrollPane) {
-                if (console) {
-                    if (console.error) {
-                        console.error('jQuery plugin "jScrollPane" not installed');
-                    } else {
-                        console.log('jQuery plugin "jScrollPane" not installed');
-                    }
-                }
-                return false;
-            }
-            scroll.jScrollPane();
-        },
-
-        applyEvents: function() {
-            var object = this;
-            var data = $(this).data();
-
-            $(data.dom.text).unbind('click');
-            $(document).off(data.dom.items);
-
-            if (data.disabled === false) {
-                $(data.dom.text).bind('click', function() {
-                    if (data.settings.onClick) {
-                        data.settings.onClick($(this));
-                    }
-                    methods.toggle.call(object);
-                });
-
-                $(data.dom.items).on('click', function() {
-                    data.index = $(this).index();
-                    if (data.settings.onSelect) {
-                        data.settings.onSelect($(this));
-                    }
-                    data.dom.input.val($(this).attr('rel'));
-                    data.dom.textElement.text($(this).text());
-                    data.dom.options.hide();
-                    data.dom.input.change();
-                    $(object).data(data);
-                });
-            }
-
-            $(data.dom.items).on('mouseover', function() {
-                $(this).addClass('hover');
-            });
-            $(data.dom.items).on('mouseleave', function() {
-                $(this).removeClass('hover');
-            });
-
-            $(document).click(function() {
-                data.dom.options.hide();
-            });
-            data.dom.container.click(function(event) {
-                event.stopPropagation();
-            });
-            $(document).keyup(function(event) {
-                if (event.keyCode == 27) {
-                    data.dom.options.hide();
-                }
-            });
-        },
-
-        open: function() {
-            var data = $(this).data();
-            if (data.settings.onOpen) {
-                data.settings.onOpen($(this));
-            }
-            if (data.settings.closeOthers) {
-                $('.customSelectOptions').hide();
-            }
-            data.dom.options.show();
-        },
-
-        close: function() {
-            var data = $(this).data();
-            if (data.settings.onClose) {
-                data.settings.onClose($(this));
-            }
-            data.dom.options.hide();
-        },
-
-        toggle: function() {
-            var data = $(this).data();
-            if (data.dom.options.is(':visible')) {
-                methods.close.call(this);
-            } else {
-                methods.open.call(this);
-            }
-        }
-    };
-
-    $.fn.customSelect = function(request) {
-        if (methods[request]) {
-            return methods[request].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof request === 'object' || !request) {
-            return methods.init.apply(this, arguments);
-        } else {
-            $.error('Method ' + request + ' does not exist on jQuery.customSelect');
-        }
-    };
-
-})(jQuery);
+});
